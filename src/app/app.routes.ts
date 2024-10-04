abf049a004ec6e4f6d71a11589b1ed257ef53da8
mstat777 <statevd@gmail.com>
2024-10-04 22:59:39 +0200

    products and detail pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,14 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { TestComponent } from './pages/test/test.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HoneyComponent } from './pages/honey/honey.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'honey', component: HoneyComponent },
+    { path: 'honey/detail/01', component: DetailComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'test', component: TestComponent },
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
     baseUrl: 'http://localhost:4200',
-    production: false,
-    mapbox: {
-        accessToken: 'pk.eyJ1IjoiYnJhc2thbSIsImEiOiJja3NqcXBzbWoyZ3ZvMm5ybzA4N2dzaDR6In0.RUAYJFnNgOnn80wXkrV9ZA',
-    }
+    production: false
 }
